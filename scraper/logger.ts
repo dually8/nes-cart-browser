cf5c6453741c11e0b3dc53c89610f7c64e1ad09f
CJ Coffey <cj@cjcoffey.com>
2024-12-13 16:19:57 -0500

    chore(prettier): run prettier formatter on repo

--- scraper/logger.ts
+++ scraper/logger.ts
@@ -1,43 +1,43 @@
 export class Logger {
-  private static instance: Logger;
-  private logLevel: LogLevel;
+	private static instance: Logger;
+	private logLevel: LogLevel;
 
-  private constructor(logLevel: LogLevel = LogLevel.INFO) {
-    this.logLevel = logLevel;
-  }
+	private constructor(logLevel: LogLevel = LogLevel.INFO) {
+		this.logLevel = logLevel;
+	}
 
-  public static getInstance(): Logger {
-    if (!Logger.instance) {
-      Logger.instance = new Logger();
-    }
-    return Logger.instance;
-  }
+	public static getInstance(): Logger {
+		if (!Logger.instance) {
+			Logger.instance = new Logger();
+		}
+		return Logger.instance;
+	}
 
-  public setLogLevel(logLevel: LogLevel): void {
-    this.logLevel = logLevel;
-  }
+	public setLogLevel(logLevel: LogLevel): void {
+		this.logLevel = logLevel;
+	}
 
-  public info(message: string, ...args: unknown[]): void {
-    if (this.logLevel <= LogLevel.INFO) {
-      console.log(`[INFO] ${message}`, args);
-    }
-  }
+	public info(message: string, ...args: unknown[]): void {
+		if (this.logLevel <= LogLevel.INFO) {
+			console.log(`[INFO] ${message}`, args);
+		}
+	}
 
-  public warn(message: string, ...args: unknown[]): void {
-    if (this.logLevel <= LogLevel.WARN) {
-      console.warn(`[WARN] ${message}`, args);
-    }
-  }
+	public warn(message: string, ...args: unknown[]): void {
+		if (this.logLevel <= LogLevel.WARN) {
+			console.warn(`[WARN] ${message}`, args);
+		}
+	}
 
-  public error(message: string, ...args: unknown[]): void {
-    if (this.logLevel <= LogLevel.ERROR) {
-      console.error(`[ERROR] ${message}`, args);
-    }
-  }
+	public error(message: string, ...args: unknown[]): void {
+		if (this.logLevel <= LogLevel.ERROR) {
+			console.error(`[ERROR] ${message}`, args);
+		}
+	}
 }
 
 export enum LogLevel {
-  INFO,
-  WARN,
-  ERROR,
+	INFO,
+	WARN,
+	ERROR,
 }
--- scraper/scraper.ts
+++ scraper/scraper.ts
@@ -14,15 +14,15 @@ const IMG_TO_SAVE_SELECTOR = '.content img';
 const logger = Logger.getInstance();
 
 type GameLink = {
-  title: string;
-  link: string;
+	title: string;
+	link: string;
 };
 
 type GameDetails = {
-  gameTitle: string;
-  catalogId: string;
-  region: string;
-  releaseDate: string;
+	gameTitle: string;
+	catalogId: string;
+	region: string;
+	releaseDate: string;
 };
 
 /**
@@ -33,122 +33,121 @@ type GameDetails = {
  * @returns cleaned string
  */
 function cleanString(str: string): string {
-  return str.trim().replace(/\s|\u00a0/g, '');
+	return str.trim().replace(/\s|\u00a0/g, '');
 }
 
 async function getGameLinks(page: playwright.Page): Promise<GameLink[]> {
-  return await page.locator(RESULT_SELECTOR).evaluateAll((elements, _baseUrl) => {
-    return elements.map((el) => {
-      return {
-        title: el.textContent as string,
-        link: _baseUrl + el.getAttribute('href') as string,
-      };
-    });
-  }, BASE_URL);
+	return await page.locator(RESULT_SELECTOR).evaluateAll((elements, _baseUrl) => {
+		return elements.map((el) => {
+			return {
+				title: el.textContent as string,
+				link: (_baseUrl + el.getAttribute('href')) as string,
+			};
+		});
+	}, BASE_URL);
 }
 
 async function getGameDetails(page: playwright.Page, game: GameLink): Promise<GameDetails> {
-  // get the first .headingmain element and get the text content
-  const gameTitle = game.title;
-  logger.info('Game title:', gameTitle);
-  // find a th containing "Catalog ID" and get the text of the sibling td
-  const catalogId = await page.locator('th:has-text("Catalog ID") ~ td').textContent() ?? 'null';
-  logger.info('Catalog ID:', catalogId);
-  // find a th containing "Region" and get the text of the sibling td
-  const region = await page.locator('th:has-text("Region") ~ td').textContent() ?? 'null';
-  logger.info('Region:', region);
-  // find a th containing "Release Date" and get the text of the sibling td
-  const releaseDate = await page.locator('th:has-text("Release Date") ~ td').textContent() ?? 'null';
-  logger.info('Release Date:', releaseDate);
-  return { gameTitle, catalogId, region, releaseDate };
+	// get the first .headingmain element and get the text content
+	const gameTitle = game.title;
+	logger.info('Game title:', gameTitle);
+	// find a th containing "Catalog ID" and get the text of the sibling td
+	const catalogId = (await page.locator('th:has-text("Catalog ID") ~ td').textContent()) ?? 'null';
+	logger.info('Catalog ID:', catalogId);
+	// find a th containing "Region" and get the text of the sibling td
+	const region = (await page.locator('th:has-text("Region") ~ td').textContent()) ?? 'null';
+	logger.info('Region:', region);
+	// find a th containing "Release Date" and get the text of the sibling td
+	const releaseDate =
+		(await page.locator('th:has-text("Release Date") ~ td').textContent()) ?? 'null';
+	logger.info('Release Date:', releaseDate);
+	return { gameTitle, catalogId, region, releaseDate };
 }
 
 async function saveGameImage(page: playwright.Page, catalogId: string) {
-  const cartImg = page.locator(CART_FRONT_SELECTOR);
-  const hasImg = await cartImg.locator('a').count() > 0;
-  logger.info('Has Img:', hasImg);
-  if (hasImg) {
-    const imgUrl = await page.locator(CART_IMG_SELECTOR).getAttribute('href') as string;
-    const fullImgUrl = BASE_URL + imgUrl;
-    logger.info('Going to', fullImgUrl);
-    // go to the imgUrl
-    await page.goto(fullImgUrl);
-    // get the src of the imgToSaveSelector
-    const imgSrc = await page.locator(IMG_TO_SAVE_SELECTOR).getAttribute('src');
-    logger.info('Image Src:', imgSrc);
-    // save the image
-    const imgPath = path.join(ASSETS_DIR, `${catalogId}.png`);
-    const imgResponse = await fetch(
-      `${BASE_URL}${imgSrc}`
-    );
-    const imgBuffer = await imgResponse.arrayBuffer();
-    fs.writeFileSync(imgPath, Buffer.from(imgBuffer));
-  }
+	const cartImg = page.locator(CART_FRONT_SELECTOR);
+	const hasImg = (await cartImg.locator('a').count()) > 0;
+	logger.info('Has Img:', hasImg);
+	if (hasImg) {
+		const imgUrl = (await page.locator(CART_IMG_SELECTOR).getAttribute('href')) as string;
+		const fullImgUrl = BASE_URL + imgUrl;
+		logger.info('Going to', fullImgUrl);
+		// go to the imgUrl
+		await page.goto(fullImgUrl);
+		// get the src of the imgToSaveSelector
+		const imgSrc = await page.locator(IMG_TO_SAVE_SELECTOR).getAttribute('src');
+		logger.info('Image Src:', imgSrc);
+		// save the image
+		const imgPath = path.join(ASSETS_DIR, `${catalogId}.png`);
+		const imgResponse = await fetch(`${BASE_URL}${imgSrc}`);
+		const imgBuffer = await imgResponse.arrayBuffer();
+		fs.writeFileSync(imgPath, Buffer.from(imgBuffer));
+	}
 }
 
 async function scrapeGames() {
-  const nesCarts: NesCartItem[] = [];
-  const browser = await playwright.firefox.launch({
-    headless: false,
-  })
-  const context = await browser.newContext();
-  const page = await context.newPage();
+	const nesCarts: NesCartItem[] = [];
+	const browser = await playwright.firefox.launch({
+		headless: false,
+	});
+	const context = await browser.newContext();
+	const page = await context.newPage();
 
-  // First, get the initial page of games
-  await page.goto(SEARCH_URL);
-  const basePageUrl = 'https://nescartdb.com/search/advanced?region_op=equal&region=USA&page=';
-  // const pageLinks = Array.from({length: 15}, (_, i) => basePageUrl + (i + 2));
-  const pageLinks = Array.from({length: 1}, (_, i) => basePageUrl + (i + 2));
-  let games = await getGameLinks(page);
-  for (const game of games) {
-    logger.info('Going to', game.link);
-    await page.goto(game.link);
-    const { gameTitle, catalogId, region, releaseDate } = await getGameDetails(page, game);
-    await saveGameImage(page, catalogId);
-    nesCarts.push({
-      catalogId,
-      region: cleanString(region),
-      coverPhotoUrl: `/assets/${catalogId}.png`,
-      releaseDate,
-      title: gameTitle,
-    });
-  }
+	// First, get the initial page of games
+	await page.goto(SEARCH_URL);
+	const basePageUrl = 'https://nescartdb.com/search/advanced?region_op=equal&region=USA&page=';
+	// const pageLinks = Array.from({length: 15}, (_, i) => basePageUrl + (i + 2));
+	const pageLinks = Array.from({ length: 1 }, (_, i) => basePageUrl + (i + 2));
+	let games = await getGameLinks(page);
+	for (const game of games) {
+		logger.info('Going to', game.link);
+		await page.goto(game.link);
+		const { gameTitle, catalogId, region, releaseDate } = await getGameDetails(page, game);
+		await saveGameImage(page, catalogId);
+		nesCarts.push({
+			catalogId,
+			region: cleanString(region),
+			coverPhotoUrl: `/assets/${catalogId}.png`,
+			releaseDate,
+			title: gameTitle,
+		});
+	}
 
-  // Then, go through the list of pageLinks and do the same
-  for (const link of pageLinks) {
-    logger.info('Going to', link);
-    await page.goto(link);
-    games = await getGameLinks(page);
-    for (const game of games) {
-      logger.info('Going to', game.link);
-      await page.goto(game.link);
-      const { gameTitle, catalogId, region, releaseDate } = await getGameDetails(page, game);
-      await saveGameImage(page, catalogId);
-      nesCarts.push({
-        catalogId,
-        region,
-        coverPhotoUrl: `/assets/${catalogId}.png`,
-        releaseDate,
-        title: gameTitle,
-      });
-    }
-  }
+	// Then, go through the list of pageLinks and do the same
+	for (const link of pageLinks) {
+		logger.info('Going to', link);
+		await page.goto(link);
+		games = await getGameLinks(page);
+		for (const game of games) {
+			logger.info('Going to', game.link);
+			await page.goto(game.link);
+			const { gameTitle, catalogId, region, releaseDate } = await getGameDetails(page, game);
+			await saveGameImage(page, catalogId);
+			nesCarts.push({
+				catalogId,
+				region,
+				coverPhotoUrl: `/assets/${catalogId}.png`,
+				releaseDate,
+				title: gameTitle,
+			});
+		}
+	}
 
-  await browser.close();
-  return nesCarts;
+	await browser.close();
+	return nesCarts;
 }
 
 async function main() {
-  try {
-    logger.setLogLevel(LogLevel.INFO);
-    const nesCarts = await scrapeGames();
-    // Write the contents of nesCarts to a json file
-    const jsonPath = path.join(ASSETS_DIR, 'nesCarts.json');
-    fs.writeFileSync(jsonPath, JSON.stringify(nesCarts, null, 2));
-    logger.info('Done');
-  } catch (e) {
-    console.error(e);
-  }
+	try {
+		logger.setLogLevel(LogLevel.INFO);
+		const nesCarts = await scrapeGames();
+		// Write the contents of nesCarts to a json file
+		const jsonPath = path.join(ASSETS_DIR, 'nesCarts.json');
+		fs.writeFileSync(jsonPath, JSON.stringify(nesCarts, null, 2));
+		logger.info('Done');
+	} catch (e) {
+		console.error(e);
+	}
 }
 
 main();
